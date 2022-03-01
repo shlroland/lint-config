8ba37d4845d40366835b7346e62b22738920b121
shlroland <shlroland1995@gmail.com>
2022-03-01 23:43:47 +0800

    feat: 添加 prettier 配置

---
+++ packages/prettier/index.ts
@@ -0,0 +1,10 @@
+import type { Options } from 'prettier'
+
+const config: Options = {
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+}
+
+export default config
+module.exports = config
---
+++ packages/prettier/lib/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    singleQuote: true,
+    trailingComma: 'all',
+    semi: false,
+};
+exports.default = config;
+module.exports = config;
