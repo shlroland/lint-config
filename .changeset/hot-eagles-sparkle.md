457e0ff4d64f94ab1583262ad4ec78237ddbf85f
shlroland <shlroland1995@gmail.com>
2024-10-11 20:54:17 +0800

    fix: :bug: update commit linting dependencies to include @commitlint/cli for better support

---
+++ .changeset/hot-eagles-sparkle.md
@@ -0,0 +1,5 @@
+---
+"@shlroland/lint-cli": minor
+---
+
+fix `@commitlint/cli` missing
--- packages/cli/src/tasks/installer.ts
+++ packages/cli/src/tasks/installer.ts
@@ -3,7 +3,7 @@ import { LintTools } from '../constants'
 
 const lintToolsPkgs = {
   [LintTools.ESLINT]: ['eslint', '@shlroland/eslint-config', 'eslint-plugin-format'],
-  [LintTools.COMMITLINT_CZG]: ['czg', '@shlroland/cz-config'],
+  [LintTools.COMMITLINT_CZG]: ['czg', '@commitlint/cli', '@shlroland/cz-config'],
   [LintTools.LINT_STAGED]: ['lint-staged', '@shlroland/lint-staged'],
   [LintTools.HUSKY]: ['husky'],
 }
