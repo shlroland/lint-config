4d496041c106cc5878eddff002a5dfb39fd37480
shlroland <shlroland1995@gmail.com>
2024-10-22 23:26:34 +0800

    :sparkles: (cz-config): add gitmoji configuration

--- packages/cz-config/src/index.ts
+++ packages/cz-config/src/index.ts
@@ -1,2 +1,2 @@
 export * from './conventional'
-export * from './czg'
+export * from './gitmoji'
--- packages/cz-config/tsup.config.ts
+++ packages/cz-config/tsup.config.ts
@@ -3,7 +3,7 @@ import { config } from '../../tsup.config'
 
 export default defineConfig(
   config({
-    entry: ['src/index.ts', 'src/czg.ts', 'src/commitlint.ts'],
+    entry: ['src/index.ts', 'src/gitmoji.ts', 'src/conventional.ts'],
     esbuildOptions: (options) => {
       options.charset = 'utf8'
     },
